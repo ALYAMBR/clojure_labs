07a95832bd974db0db68177819d33287573549b7
rWX1047554 <radeev.nikita@huawei.com>
2021-11-24 17:43:24 +0700

    main work is done

--- clojure4/src/clojure4/core.clj
+++ clojure4/src/clojure4/core.clj
@@ -1,88 +1,281 @@
 (ns clojure4.core
   (:gen-class))
 
-; TODO constants and variables
-(defn variable
-  [name]
-  {:pre [(keyword? name)]}
-  (list ::var name)
-  )
+(defn print_expr [expr]
+  (def s0 (str expr))
+  (def s1 (clojure.string/replace s0 #":clojure4.core\/" ""))
+  (def s2 (clojure.string/replace s1 #"\(const (\d+)\)" "$1"))
+  (def s3 (clojure.string/replace s2 #"\(var :([a-zA-Z]+)\)" "$1"))
+  (println s3))
 
-(defn variable?
-  [expr]
+; constants
+(defn constant [num]
+  {:pre [(number? num)]}
+  (list ::const num))
+
+(defn constant? [expr]
+  (= (first expr) ::const))
+
+(defn constant-value [v]
+  (second v))
+
+(defn same-constants? [v1 v2]
+  (and
+   (constant? v1)
+   (constant? v2)
+   (= (constant-value v1)
+      (constant-value v2))))
+
+; variables
+(defn variable [name]
+  {:pre [(keyword? name)]}
+  (list ::var name))
+
+(defn variable? [expr]
   (= (first expr) ::var))
 
-(defn variable-name 
-  [variable]
-  (second variable))
+(defn variable-name [v]
+  (second v))
 
-(defn same-variables?
-  [var1 var2]
+(defn same-variables? [v1 v2]
   (and
-   (variable? var1)
-   (variable? var2)
-   (= (variable-name var1)
-      (variable-name var2))))
+   (variable? v1)
+   (variable? v2)
+   (= (variable-name v1)
+      (variable-name v2))))
 
-; TODO make constants
+(defn in?
+  "true if collection contains element"
+  [coll elem]
+  (some #(= elem %) coll))
 
-(defn get-value
-  [logical-expr]
-  ; TODO calculate value
-  )
+; disjunction
+(defn disjunction 
+  ([expr & rest]
+   (cons ::disj (cons expr rest)))
+  ([rest]
+   (cons ::disj rest)))
 
-; TODO logical operators
-(defn disjunction
-  [expr & rest]
-  (cons ::disjunction (cons expr rest)))
+; check a type for disjunction
+(defn disjunction? [expr]
+  (= ::disj (first expr)))
 
-(defn disjunction?
-  [expr]
-  (= ::disjunction (first expr)))
-
+; conjuction
 (defn conjuction
-  [expr & rest]
-  (cons ::conjuction (cons expr rest)))
+  ([rest]
+   (cons ::conj rest))
+  ([expr & rest]
+   (cons ::conj (cons expr rest))))
 
-(defn conjuction?
-  [expr]
-  (= ::conjuction (first expr)))
+; check a type for conjuction
+(defn conjuction? [expr]
+  (= ::conj (first expr)))
 
-(defn implication
-  [expr & rest]
-  (cons ::implication (cons expr rest)))
+; list of expr arguments
+(defn args [expr]
+  (rest expr))
 
-(defn implication?
-  [expr]
-  (= ::implication (first expr)))
+; inversed expr
+(defn inversion [expr]
+  (list ::not expr))
+; check a type for an inversed expr
+(defn inversion? [expr]
+  (= ::not (first expr)))
+(defn single_inversion? [expr]
+  (and
+     (inversion? expr)
+     (not (conjuction? (second expr)))
+     (not (disjunction? (second expr)))))
+; implication
+(defn implication [v1 & v2]
+  (cons ::impl (cons v1 v2)))
+; check a type for an implication 
+(defn implication? [expr]
+  (= ::impl (first expr)))
 
-(defn rejection
-  [expr]
-  (cons ::rejection expr))
+; check rules:
+(defn inversed_expr? [expr]
+  (and
+   (inversion? expr)
+   (or
+    (conjuction? (second expr)) 
+    (disjunction? (second expr))
+    )))
 
-(defn rejection?
-  [expr]
-  (= ::rejection (first expr)))
+(defn double_inversion? [expr]
+  (and
+   (inversion? expr)
+   (inversion? (second expr))))
 
-(defn calc-disjunction
-  [x1 x2]
-  (or (get-value x1) (get-value x2)))
+(defn distributable? [expr]
+  (and
+   (conjuction? expr)
+   (> (count
+       (filter
+        #(= ::disj (first %))
+        (args expr)))
+      0)))
 
-(defn calc-conjuction
-  [x1 x2]
-  (and (get-value x1) (get-value x2)))
-
-(defn calc-rejection
-  [x]
-  (not (get-value x)))
-
-(defn calc-implication
-  [x1 x2]
-  (or (not (get-value x1)) (get-value x2)))
-
-; TODO simplification rules
-
+; utils
+(defn concat_conj [expr & rest]
+  (let [concated_expr (cons expr rest)]
+    (if (= 1 (count concated_expr))
+      (first concated_expr)
+      (cons ::conj concated_expr))))
 
 
-(def C1 (constant true))
-println(C1)
+(defn concat_disj [expr & rest]
+  (let [concated_expr (cons expr rest)]
+    (if (= 1 (count concated_expr))
+      (first concated_expr)
+      (cons ::disj concated_expr))))
+
+
+(defn append_disj_to_conj [expr disj_list]
+  (let [top_operation (first expr)
+        expr_args (case top_operation
+                    ::not (list expr)
+                    ::impl expr
+                    ::disj (args expr)
+                    ::conj (args expr)
+                    (list expr))
+        disj_list_args (if (variable? disj_list)
+                         disj_list
+                         (args disj_list))
+        result (map
+                #(apply conjuction (concat expr_args (list %)))
+                disj_list_args)]
+    result))
+
+
+(declare dnf)
+(declare my_dnf)
+
+; transformation rules
+(def dnf_rules
+  (list
+        ; replace all exotic operations with disj, conj, inversion.
+   [(fn [expr] (implication? expr))
+    (fn [expr]
+      (let [first-arg (inversion (first (args expr)))
+            second-arg (second (args expr))
+            result (disjunction
+                    first-arg
+                    second-arg)]
+        result))]
+
+        ; put inversion inside to atomic logic parts
+   [(fn [expr] (inversed_expr? expr))
+    (fn [expr](if (conjuction? (second expr))
+          (apply disjunction
+                 (map
+                  inversion
+                  (args (second expr))))
+
+          (apply conjuction
+                 (map
+                  inversion
+                  (args (second expr))))))]
+
+        ; remove double inversion
+   [(fn [expr] (double_inversion? expr))
+    (fn [expr] (second (second expr)))]
+
+        ; distribution
+   [(fn [expr] (distributable? expr))
+    (fn [expr]
+      (let [disj_list (first
+             (filter
+              #(= ::disj (first %))
+              (args expr)))
+            
+            other (remove
+             #(= disj_list %)
+             (args expr))
+            
+            result (apply 
+                    disjunction 
+                    (first 
+                     (map
+                      #(append_disj_to_conj % disj_list)
+                      other)))]
+        result))]
+
+        ; stop of reccursion
+   [(fn [expr] (constant? expr))
+    (fn [expr] expr)]
+
+   [(fn [expr] (variable? expr))
+    (fn [expr] expr)]
+
+
+   [(fn [expr] (single_inversion? expr))
+    (fn [expr]
+      (let [result (inversion (dnf (second expr)))]
+        result))]
+
+   [(fn [expr] (inversion? expr))
+    (fn [expr] expr)]
+
+
+   [(fn [expr] (conjuction? expr))
+    (fn [expr]
+      (let [new_expr (distinct expr)
+            contain_inversion
+            (boolean
+             (some
+              #(in? (args new_expr) (inversion %))
+              (args new_expr)))
+            contain_false
+            (boolean
+             (in?
+              (args new_expr)
+              (constant 0)))]
+        (if (or
+             contain_inversion
+             contain_false)
+          (constant 0)
+          (apply  concat_conj
+                  (map
+                   #(dnf %)
+                   (args new_expr))))))]
+
+   [(fn [expr] (disjunction? expr))
+    (fn [expr]
+      (let [new_expr (distinct expr)
+            contain_inversion
+            (boolean
+             (some
+              #(in? (args new_expr) (inversion %))
+              (args new_expr)))
+            contain_true
+            (boolean
+             (in?
+              (args new_expr)
+              (constant 1)))]
+        (if (or
+             contain_inversion
+             contain_true)
+          (constant 1)
+          (apply  concat_disj
+                  (map
+                   #(dnf %)
+                   (args new_expr))))))]))
+
+
+(defn dnf [expr]
+    (print_expr expr)
+  ((some (fn [rule]
+           (if ((first rule) expr)
+             (second rule)
+             false))
+         dnf_rules)
+   expr))
+
+
+(defn my_dnf [expr]
+  (let [prev expr curr (dnf expr)]
+    (print_expr expr)
+    (if (= prev curr)
+      curr
+      (my_dnf curr))))
--- clojure4/test/clojure4/core_test.clj
+++ clojure4/test/clojure4/core_test.clj
@@ -2,6 +2,215 @@
   (:require [clojure.test :refer :all]
             [clojure4.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test_private_functions
+  (println "------------------------")
+  (testing "Метод нужный при реализации дистрибутивности")
+  (is (=
+       (list
+        (conjuction
+         (variable :A)
+         (variable :B)
+         (variable :C))
+        (conjuction
+         (variable :A)
+         (variable :B)
+         (variable :D)))
+
+       (append_disj_to_conj
+        (conjuction
+         (variable :A)
+         (variable :B))
+        (disjunction
+         (variable :C)
+         (variable :D))))))
+
+
+(deftest test_no_recurion
+  (println "------------------------")
+  (testing "Замена импликации на +,*,`")
+  (is (=
+       (disjunction
+        (inversion (variable :A))
+        (variable :B))
+       (my_dnf
+        (implication
+         (variable :A)
+         (variable :B)))))
+  (println "------------------------")
+  (is (=
+       (disjunction
+        (variable :A)
+        (variable :B))
+       (my_dnf
+        (implication
+         (inversion (variable :A))
+         (variable :B)))))
+
+  (testing "Раскрытие отрицание от выражения")
+  (println "------------------------")
+  (is (=
+       (conjuction
+        (inversion (variable :A))
+        (inversion (variable :B)))
+       (my_dnf
+        (inversion
+         (disjunction
+          (variable :A)
+          (variable :B))))))
+  (println "------------------------")
+  (is (=
+       (disjunction
+        (inversion (variable :A))
+        (inversion (variable :B)))
+       (my_dnf
+        (inversion
+         (conjuction
+          (variable :A)
+          (variable :B))))))
+  (println "------------------------")
+  (testing "Избавляемся от знаков двойного отрицания")
+  (is (=
+       (conjuction
+        (variable :A)
+        (variable :B))
+       (my_dnf
+        (inversion       ; 2 лишних
+         (inversion   ; отрицания
+          (conjuction
+           (variable :A)
+           (variable :B)))))))
+  (println "------------------------")
+  (is (=
+       (conjuction
+        (variable :A)
+        (variable :B))
+       (my_dnf
+        (inversion               ; 4 лишних
+         (inversion           ; отрицания
+          (inversion       ; 4 лишних
+           (inversion   ; отрицания
+            (conjuction
+             (variable :A)
+             (variable :B)))))))))
+  (println "------------------------")
+  (testing "Используем закон дистрибутивности")
+  (is (=
+       (disjunction
+        (conjuction
+         (variable :A)
+         (variable :B)
+         (variable :C))
+        (conjuction
+         (variable :A)
+         (variable :B)
+         (variable :D)))
+       (my_dnf
+        (conjuction
+         (conjuction
+          (variable :A)
+          (variable :B))
+         (disjunction
+          (variable :C)
+          (variable :D))))))
+
+  (println "------------------------")
+  (testing "Идемпотентность конъюнкции и дизъюнкции")
+  (is (=
+       (conjuction
+        (variable :X)
+        (inversion (variable :Y)))
+       (my_dnf
+        (conjuction
+         (variable :X)
+         (inversion (variable :Y))
+         (inversion (variable :Y))))))
+  (println "------------------------")
+  (is (=
+       (disjunction
+        (variable :X)
+        (inversion (variable :Y)))
+       (my_dnf
+        (disjunction
+         (variable :X)
+         (inversion (variable :Y))
+         (inversion (variable :Y))))))
+
+  (println "------------------------")
+  (testing "Закон исключающего третьего")
+  (is (=
+       (constant 1)
+       (my_dnf
+        (disjunction
+         (variable :X)
+         (inversion (variable :X))))))
+
+  (println "------------------------")
+  (testing "Закон противоречия")
+  (is (=
+       (constant 0)
+       (my_dnf
+        (conjuction
+         (variable :X)
+         (inversion (variable :X)))))))
+
+
+(deftest test_complex_example
+  (println "------------------------")
+  (testing "Составной пример с википедии")
+  (is (=
+       (disjunction
+        (conjuction
+         (variable :X)
+         (inversion (variable :Y)))
+        (conjuction
+         (variable :X)
+         (inversion (variable :Y))
+         (variable :Z)))
+       (my_dnf
+        (inversion
+         (disjunction
+          (implication
+           (variable :X)
+           (variable :Y))
+          (inversion
+           (implication
+            (variable :Y)
+            (variable :Z)))))))))
+
+
+(deftest test_const
+  (println "------------------------")
+  (testing "проверяем, что (1 or ... = 1)")
+  (is (=
+       (constant 1)
+       (my_dnf
+        (disjunction
+         (constant 1)
+         (inversion
+          (disjunction
+           (implication
+            (variable :X)
+            (variable :Y))
+           (inversion
+            (implication
+             (variable :Y)
+             (variable :Z)))))))))
+  (println "------------------------")
+  (testing "проверяем, что (0 and ... = 0)")
+  (is (=
+       (constant 0)
+       (my_dnf
+        (conjuction
+         (constant 0)
+         (inversion
+          (disjunction
+           (implication
+            (variable :X)
+            (variable :Y))
+           (inversion
+            (implication
+             (variable :Y)
+             (variable :Z))))))))))
+
+
+(run-tests)
